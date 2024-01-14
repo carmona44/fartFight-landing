2a59314cd5f303b20ade3d1e46019166b9c956fe
Daniel <dcacarmona44@gmail.com>
2024-01-14 11:37:46 +0100

    RESOLVE: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'FartFight',
-  description: 'Aplicación de peos',
+  description: 'Aplicación de pedos',
 }
 
 export default function RootLayout({
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   )
 }
